92c36ec7a7413c8e35c685e1f5738b35daae14f3
Yamuna <Yamuna@LAPTOP-3K7ML83B>
2023-07-19 20:59:04 +0530

    Project-1

---
+++ src/com/nit/hk/main/Calc.java
@@ -0,0 +1,15 @@
+package com.nit.hk.main;
+
+import com.nit.hk.blogic.Addition;
+
+public class Calc {
+public static void main(String[] args) {
+	 Addition.add(50,60);
+	System.out.println(Addition.add(50,60));
+}
+}
+
+
+
+
+
